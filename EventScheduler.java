208a1c6458fcaf0e14edd867426b6071625ee0a2
Vivek Kumar Vinay <113384897+Vivek96254@users.noreply.github.com>
2023-10-22 14:37:16 +0530

    Add files via upload
    The interface for the appointment scheduler

---
+++ EventScheduler.java
@@ -0,0 +1,14 @@
+package DSAProject;
+import java.util.*;
+
+public interface EventScheduler {
+    void scheduleAppointment(Scanner scanner, List<Appointment> appointments, Queue<Appointment> appointmentQueue);
+
+    void viewAppointments(Queue<Appointment> appointmentQueue);
+
+    void saveAppointmentsToFile(List<Appointment> appointments);
+
+    void completeAndRemoveAppointment(Queue<Appointment> appointmentQueue, List<Appointment> appointments);
+
+    void loadAppointmentsFromFile(List<Appointment> appointments, Queue<Appointment> appointmentQueue);
+}
